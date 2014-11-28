61ca93607dc9fa80e14f72f3a49e854ff7d34ecf
Aaron Craig <aaroncraig@Dr-Cool-2.local>
2014-11-28 13:01:56 +1300

    added function for creating an interactive legend for multiple time series

--- craigpy.py
+++ craigpy.py
@@ -1,6 +1,7 @@
 from pandas import DataFrame, Series
 import pandas as pd
 import numpy as np
+from matplotlib import patches as mpatches
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -38,3 +39,44 @@ def plot_barchart(df, dates, normed=True, autoformat=True):
         ax.set_ylim(0)
         ax.set_yticks([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
     return ax
+
+def interactive_legend(ax,names,colours):
+    ''' create and return an interactive legend for the specified axis. You can click on legends to hide/show the corresponding series.
+            ax: axis for which you are creating the interactive legend.
+            names: list of names for the legend keys.
+            colours: list of colours for the legend keys. '''
+    
+    # make legend keys
+    patches = [ mpatches.Patch(facecolor=colours[i], edgecolor="black", linewidth=2.0, label=names[i])
+                for i in range(len(names)) ]
+    
+    # legend formatting
+    leg = ax.legend(loc='upper center', bbox_to_anchor=(0.5,1.05),
+                    ncol=5, fancybox=True, shadow=True, handles=patches)
+    
+    # make a mapping of legend -> line
+    linemap = {}
+    lines = ax.get_lines()
+    patches = leg.get_patches()
+    for patch, line in zip(patches, lines):
+        patch.set_picker(5)
+        linemap[patch] = line
+        
+    # create pick event for legend
+    def onpick_legends(event):
+        ''' a pick event for legend
+            when you click a legend it toggles visibility for the corresponding series '''
+        patch = event.artist
+        line = linemap[patch]
+        vis = not line.get_visible()
+        line.set_visible(vis)
+        if vis:
+            patch.set_alpha(1.0)
+        else:
+            patch.set_alpha(0.4)
+        fig.canvas.draw()
+    
+    # activate pick event for legend on the figure
+    fig = ax.get_figure()
+    fig.canvas.mpl_connect("pick_event", onpick_legends)
+    return leg
