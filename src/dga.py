4d1a9d2697760bc8f176fc3c3e17495023dce6eb
Aaron Craig <sinroth@gmail.com>
2015-01-14 09:35:06 +1300

    fix bug where not returning dict

--- src/dga.py
+++ src/dga.py
@@ -141,6 +141,7 @@ def KL_distance(test_domains, good_domains, botnet_domains, degree, alphabet="al
 				main_dist[key] = main_dist[key] + word_dist[key]
 		for key in main_dist:
 			main_dist[key] = 1.0 * main_dist[key] / len(words)
+		return main_dist
 
 	# get distributions for each collection
 	dist_good = get_dist(good_domains)
