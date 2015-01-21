50f749437c51fc08c63f605a9ae5ced9a0b1cc52
Aaron Craig <sinroth@gmail.com>
2015-01-21 13:41:36 +1300

    added domain name resolution to dga

--- src/dga.py
+++ src/dga.py
@@ -1,5 +1,6 @@
 import pycountry as __pc__
 import ling
+import socket
 
 __slds__ = ["ac", "co", "geek", "gen", "kiwi", "maori", "net", "org", "school", "cri", "govt", "health", "iwi", "mil", "parliament"]
 
@@ -46,7 +47,7 @@ def get_lld(domain):
 	'''
 	return domain.split(".")[0]
 
-def get_country(countrycode, official_name=False):
+def get_countrycode(countrycode, official_name=False):
 	'''
 	Given a country code, return the corresponding country.
 
@@ -86,35 +87,28 @@ def strip_hld(domain):
 		stripped = stripped[:-1]
 	return ".".join(stripped)
 
-def KL_test(test_distribution, good_distribution, botnet_distribution, degree, alphabet="alphanumeric"):
+def does_domain_resolve(domain):
 	'''
-	Perform the symmetric Kullback-Leibler divergence test on the given test domains.
-	Return true if the test_distribution is closer to the botnet_distribution than to the good_distribution.
+	Check if the given domain resolves to an IP address.
+	Return : bool
 
 	Parameters
 	----------
-		test_domains : Iterable
-			collection of domains you want to test with Kullback-Leibler divergence
-		good_domains : Iterable
-			collection of domains known to be legitimate
-		botnet_domains : Iterable
-			collection of domains known to be algorithmically generated
-		degree : int
-			degree of n-grams to tests (e.g.: unigrams = 1, bigrams = 2)
-			should be a positive integer
-
-	Keyword Arguments
-	-----------------
-		alphabet : "alpha" or "numeric" or "alphanumeric"
-			possible chars
+		domain : str
+			domain name to check
 	'''
+	try:
+		socket.gethostbyname(domain)
+		return True
+	except socket.gaierror:
+		return False
 
-	# get distributions for each collection
-	dist_good = get_dist(good_domains)
-	dist_botnet = get_dist(botnet_domains)
-	dist_test = get_dist(test_domains)
-
-	# compare distance between test distribution and the good/bad distributions
-	d_qg = ling.kullback_leibler_distance(dist_test, dist_good, degree, alphabet)
-	d_qb = ling.kullback_leibler_distance(dist_test, dist_botnet, degree, alphabet)
-	return d_qg > d_qb
+def ip_from_domain(domain):
+	'''
+	Return the IP address of the given domain.
+	Return None if there isn't one.
+	'''
+	try:
+		return socket.gethostbyname(domain)
+	except socket.gaierror:
+		return Nonex
